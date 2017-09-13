0194751ffd383d281be35b34ede381625f4906e7
Islam Mahmoud <eslam200788@gmail.com>
2017-09-13 15:45:38 +0200

    13/9

--- js/plagin.js
+++ js/plagin.js
@@ -1,1 +1,12 @@
 /*global $, alert, console, mixer */
+$(document).ready(function() {
+    'use strict';
+    $('.cus').click(function() {
+
+        $('aside').toggleClass('hidden-aside');
+        $('.nav-tabel').toggleClass('add-width');
+
+
+
+    });
+});
